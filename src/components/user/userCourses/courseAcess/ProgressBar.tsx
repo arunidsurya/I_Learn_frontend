aefd2281718ac272b80ceb23bf73f9bba3f7fdf2
Arun <arunid.surya@gmail.com>
2024-05-31 10:49:51 +0200

    progress bar code updated

--- src/components/user/userCourses/courseAcess/ProgressBar.tsx
+++ src/components/user/userCourses/courseAcess/ProgressBar.tsx
@@ -5,11 +5,12 @@ type Props={
 }
 
 const ProgressBar:React.FC<Props> = ({ progress}) => {
+  const cappedProgress = Math.min(progress, 100);
   return (
     <div className="w-full bg-gray-200 rounded-full h-4">
       <div
         className="bg-blue-600 h-4 rounded-full"
-        style={{ width: `${progress}%` }}
+        style={{ width: `${cappedProgress}%` }}
       ></div>
     </div>
   );
